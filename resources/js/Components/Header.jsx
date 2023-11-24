7d48551eb02fc498d171a60d1c9be083cfba12e5
Katerina Bouchalova <bouchalovakaterina1@gmail.com>
2023-11-24 09:40:32 +0100

    burger menu done

--- resources/js/Components/Header.jsx
+++ resources/js/Components/Header.jsx
@@ -6,59 +6,56 @@ import Logout from "./Logout";
 
 export default function Header() {
     const location = useLocation();
-    // const [open, setOpen] = useState(false);
+    const [open, setOpen] = useState(false);
 
-    // const toggleMenu = () => {
-    //     setOpen(!open);
-    // };
+    const toggleMenu = () => {
+        setOpen(!open);
+    };
 
     return (
         <div className="header">
-            <img src={logo} alt="MOST ProTibet" width="143" height="70" /> 
-            
-            
-                <nav>
-                    <Link
-                        to="/home"
-                        className={location.pathname === "/home" ? "active" : ""}
-                    >
-                        Home
-                    </Link>
-                    <Link
-                        to="/list"
-                        className={location.pathname === "/list" ? "active" : ""}
-                    >
-                        People I support
-                    </Link>
-                    <Link
-                        to="/news"
-                        className={location.pathname === "/news" ? "active" : ""}
-                    >
-                        News
-                    </Link>
-                    <Link
-                        to="/faq"
-                        className={location.pathname === "/faq" ? "active" : ""}
-                    >
-                        FAQ
-                    </Link>
-                    <Link
-                        to="/contact"
-                        className={location.pathname === "/contact" ? "active" : ""}
-                    >
-                        Contact
-                    </Link>
-                   <Logout />
-                </nav> 
-                
-           
-                <div className="burger-menu">
-                    <div className="div div1"></div>
-                    <div className="div div2"></div>
-                    <div className="div div3"></div>
-                </div>
-           
-           
+            <img src={logo} alt="MOST ProTibet" width="143" height="70" />
+
+            <nav className={open ? "show" : ""}>
+                <Link
+                    to="/home"
+                    className={location.pathname === "/home" ? "active" : ""}
+                >
+                    Home
+                </Link>
+                <Link
+                    to="/list"
+                    className={location.pathname === "/list" ? "active" : ""}
+                >
+                    People I support
+                </Link>
+                <Link
+                    to="/news"
+                    className={location.pathname === "/news" ? "active" : ""}
+                >
+                    News
+                </Link>
+                <Link
+                    to="/faq"
+                    className={location.pathname === "/faq" ? "active" : ""}
+                >
+                    FAQ
+                </Link>
+                <Link
+                    to="/contact"
+                    className={location.pathname === "/contact" ? "active" : ""}
+                >
+                    Contact
+                </Link>
+                <Logout />
+            </nav>
+
+            <div className={`burger-menu ${open ? "open" : ""}`} onClick={toggleMenu}>
+                <div className="div"></div>
+                <div className="div"></div>
+                <div className="div"></div>
+            </div>
         </div>
     );
+
 }
